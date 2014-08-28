d2b512f77b538aab684673159487074bbcdf0150
jagi <luke.jagodzinski@gmail.com>
2014-08-28 12:41:20 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,4 @@
+meteor-google-oauth-jwt
+=======================
+
+Implementation of Google OAuth 2.0 for server-to-server interactions
---
+++ package.js
@@ -0,0 +1,21 @@
+Package.describe({
+  summary: "Implementation of Google OAuth 2.0 for server-to-server interactions",
+  version: "1.0.0",
+  git: "https://github.com/jagi/meteor-google-oauth-jwt.git"
+});
+
+Package.onUse(function(api) {
+  api.versionsFrom('METEOR@0.9.0.1');
+  
+  api.use(['underscore', 'http']);
+  
+  api.addFiles(['lib/google-oauth-jwt.js', 'lib/http-jwt.js'], 'server');
+  
+  api.export(['GoogleOAuthJWT', 'HTTPJWT'], 'server');
+});
+
+Package.onTest(function(api) {
+  api.use(['tinytest', 'jagi:google-oauth-jwt']);
+  
+  api.addFiles('test/tests.js');
+});
