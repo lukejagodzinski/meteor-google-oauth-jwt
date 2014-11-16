3ede03ecc646d0a71819f955731947a2878d09f2
Lukasz Jagodzinski <luke.jagodzinski@gmail.com>
2014-11-16 23:34:09 +0100

    Clean up

--- package.js
+++ package.js
@@ -1,21 +1,15 @@
 Package.describe({
   summary: "Implementation of Google OAuth 2.0 for server-to-server interactions",
-  version: "1.0.2",
+  version: "1.0.3",
   git: "https://github.com/jagi/meteor-google-oauth-jwt.git"
 });
 
 Package.onUse(function(api) {
-  api.versionsFrom('METEOR@0.9.0.1');
-  
-  api.use(['underscore', 'http']);
-  
-  api.addFiles(['lib/google-oauth-jwt.js', 'lib/http-jwt.js'], 'server');
-  
-  api.export(['GoogleOAuthJWT', 'HTTPJWT'], 'server');
-});
+  api.versionsFrom('METEOR@1.0');
 
-Package.onTest(function(api) {
-  api.use(['tinytest', 'jagi:google-oauth-jwt']);
-  
-  api.addFiles('test/tests.js');
+  api.use(['underscore', 'http']);
+
+  api.addFiles(['lib/google-oauth-jwt.js', 'lib/http-jwt.js'], 'server');
+
+  api.export(['GoogleOAuthJWT', 'HTTPJWT'], 'server');
 });
