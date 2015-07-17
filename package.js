de20eceafb9637b631b63ddf2a5a57892c6417bf
jagi <luke.jagodzinski@gmail.com>
2015-07-17 11:20:13 +0200

    v1.0.4

--- package.js
+++ package.js
@@ -1,7 +1,7 @@
 Package.describe({
-  summary: "Implementation of Google OAuth 2.0 for server-to-server interactions",
-  version: "1.0.3",
-  git: "https://github.com/jagi/meteor-google-oauth-jwt.git"
+  summary: 'Implementation of Google OAuth 2.0 for server-to-server interactions',
+  version: '1.0.4',
+  git: 'https://github.com/jagi/meteor-google-oauth-jwt.git'
 });
 
 Package.onUse(function(api) {
